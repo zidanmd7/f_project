2094a663a6a7af845797fd20013df8624d0454fe
zidanmd7 <zidan.md7@gmail.com>
2026-01-26 04:34:18 -0600

    initial setup

---
+++ f_project/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+
+function App() {
+  return(
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App
---
+++ f_project/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <p>This is a test webpage currently under work</p>
+    );
+}
+
+
+export default Footer
