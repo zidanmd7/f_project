000937f8551344494f8f6b33f32f3ad6439e9827
Ismail Mohammad <ismailabedin5@gmail.com>
2026-01-30 00:44:54 +0100

    review board created and edited

---
+++ f_project/src/Reviews_Board.jsx
@@ -0,0 +1,12 @@
+
+function Reviews_Board() {
+    return (
+        <div className="review-grid-3">
+            <div className="review">Review 1</div>
+            <div className="review"> Review 2</div>
+            <div className="review"> Review 3</div>
+        </div>
+    )
+}
+
+export default Reviews_Board;
